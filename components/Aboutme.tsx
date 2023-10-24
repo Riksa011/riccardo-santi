ac20cc4edf6677c2cb0668bcd500766c7ac5736f
Riksa01 <riccardobusiness04@gmail.com>
2023-10-24 22:20:40 +0200

    About me section updated and font packages

--- components/Aboutme.tsx
+++ components/Aboutme.tsx
@@ -7,13 +7,16 @@ const Aboutme = () => {
       <Title text="About Me" icon={<SiInformatica />} />
       <div className="text-lg tracking-wide flex flex-col gap-6">
         <p>
-        I am a Junior Blockchain Developer with experience in building Django and NextJS 
-        web applications that incorporate the benefits of blockchain technology, 
-        including the use of Solidity smart contracts.
+        🙋🏻‍♂️ I'm a 19-year-old guy passionate about blockchain and cryptocurrencies, determined to turn my passion into a career on the web.
         </p>
         <p>
-        I am actively seeking new job opportunities that will make me grow both personally 
-        and professionally and I am excited to take on exciting new challenges.
+	🔗 I believe that blockchain is a disruptive technology that will change our world and it deserves to be studied and supported.
+        </p>
+        <p>
+	👨🏻‍💻 I attended the Blockchain Development Master at Start2Impact University, working on 5 individual full-stack projects over 9 months. Through this hands-on experience, I've gained a strong foundation in web development using Django and Next.js, as well as skills in writing smart contracts in Solidity.
+        </p>
+        <p>
+	💼 I’m actively seeking new job opportunities that will make me grow both personally and professionally, and I'm excited to put my web development skills into practice 🚀.
         </p>
       </div>
     </div>
